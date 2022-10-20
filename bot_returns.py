b9341029cf61233b2b5bf2adc34665e09af71f03
davizinho <davizheng2003@gmail.com>
2022-10-20 00:14:13 -0400

    updated

--- bot_returns.py
+++ bot_returns.py
@@ -1,3 +1,12 @@
+import random
+
+# joke command which returns a random joke
+joke1 = "9 + 10 = 21"
+joke2 = "Whatever"
+joke3 = "I am a smart bot"
+jokes_list = [joke1, joke2, joke3]
+
+
 def handle_response(message) -> str:
     input_message = message.lower()
 
@@ -7,9 +16,16 @@ def handle_response(message) -> str:
     if input_message == "dance for me":
         return "https://tenor.com/view/happy-dance-dance-moves-groove-cute-gif-17919727"
 
-    if input_message == "!help":
-        return_string = "Here are a list of commands I currently hold." \
-                        "hello, dance for me, !help"
+    if input_message == "help":
+        return_string = "Here are a list of commands I currently hold. " \
+                        "!help, !fart, !joke"
         return return_string
+
+    if input_message == "fart":
+        return "https://tenor.com/view/love-fart-lovefart-you-gif-22670000"
+
+    if input_message == "joke":
+        random_number = random.randint(0, 2)
+        return jokes_list[random_number]
 
     return ""
